0fbb5c1195713782e3745ebf20418e54d8e4d004
ryuh2929 <ryuh2929@gmail.com>
2026-05-20 11:03:31 +0900

    init: tailwind, nativewind, global.css 등 설정 파일 작성

---
+++ frontend/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      "react-native-reanimated/plugin", // ✅ 반드시 맨 마지막
+    ],
+  };
+};
