7c311357f4de98dd893848249510e7564d1039e6
ryuh2929 <ryuh2929@gmail.com>
2026-05-20 12:55:32 +0900

    refactor: global.css 파일 src 폴더 안에서 frontend 바로 아래로 이동

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
   ],
 
