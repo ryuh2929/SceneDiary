a1d88081db9127e6c8d7fd619d407eda1d4a429b
user <blossom41902@gmail.com>
2026-05-29 09:09:58 +0900

    router 옵션 추가에 인해 경로 수정

--- backend/app/routers/map.py
+++ backend/app/routers/map.py
@@ -6,14 +6,14 @@ from app.db.session import get_db
 from app.schemas.map import DaysWithPhotos
 import os
 
-router = APIRouter(tags=["map"])
+router = APIRouter(tags=["map"], prefix="/map") # 만약 이렇게 되어 있다면?
 
 # ① 여행 전체 조회 
 @router.get("/trip_days", summary="지도에 모든 일별 이미지 마커 생성하기",response_model=list[DaysWithPhotos])
 def get_days(request: Request, db: Session = Depends(get_db)
 ) -> list[DaysWithPhotos]:
     # trip_days 모두 호출하기
-    all_diaries = db.query(TripDay).options(joinedload(TripDay.photos)).all()
+    all_diaries = db.query(TripDay).options(joinedload(TripDay.photos)).order_by(TripDay.trip_id,TripDay.day_number).all()
     BASE_URL = str(request.base_url).rstrip("/")
     # 2. 데이터를 순회하며 가공합니다.
     for diary in all_diaries:
